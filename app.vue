1954a244813a7568d36575e05b064f866b46ae44
Anders Dahl <mail@andersdahl.org>
2024-10-16 18:48:14 +0200

    Added mail component to app.vue.

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <band-logo />
+    <email-row />
   </div>
 </template>
---
+++ components/EmailRow.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="center-column">
+    <a href="mailto:info@skandalhuset.se">
+      BOOK US!
+    </a>
+  </div>
+</template>
+
+<style>
+
+  .center-column {
+  margin:auto;
+  width: 50%;
+  text-align: center;
+  padding: 10px;
+  }
+  </style>
